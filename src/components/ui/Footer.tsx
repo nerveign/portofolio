0bd64955e40b5aed0762090318453c9ad3c7708a
unknown <fathur.risqo@gmail.com>
2025-10-10 07:58:17 +0700

    Update text size in footer

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -3,7 +3,7 @@ import { year } from '@/lib/getYear';
 export const Footer = () => {
   return (
     <>
-      <footer className="border-t-1 text-sm md:text-base text-zinc-500 py-8 flex flex-col md:flex-row gap-2 justify-center md:justify-between lg:w-full lg:max-w-5xl mx-auto px-6">
+      <footer className="border-t-1 text-sm text-zinc-500 py-8 flex flex-col md:flex-row gap-2 justify-center md:justify-between lg:w-full lg:max-w-5xl mx-auto px-6">
         <div className="text-center lg:text-start">
           Design by{' '}
           <a className="underline" href="https://www.instagram.com/fathurrizqo/">
