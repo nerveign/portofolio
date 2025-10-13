1e46f703a79d89fc1888c21efeed0b897e3e1ac7
unknown <fathur.risqo@gmail.com>
2025-10-13 08:56:13 +0700

    Update project's data

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -12,12 +12,18 @@ export const data: Project[] = [
     imageURL: `${url}/portofolio-1.png`,
     title: 'PureCycle Mobile UI',
     description: 'Build user-flow for recycling applications, design and prototyping the UI.',
-    label: ['Strategy', 'Design', 'UX'],
+    label: ['UI', 'UX', 'Strategy', 'Prototyping'],
   },
   {
     imageURL: `${url}/portofolio-2.png`,
     title: 'UNNES Career Mobile UI',
     description: 'This application prototype combines job and career vacancy listings with online course offerings in a unified platform.',
     label: ['UI', 'UX', 'Prototyping'],
+  },
+  {
+    imageURL: `${url}/portofolio-3.png`,
+    title: 'React Tetris Game',
+    description: 'Browser-based implementation of the classic Tetris puzzle game built entirely with React and powered by Vite for development.',
+    label: ['Frontend', 'Game', 'React'],
   },
 ];
