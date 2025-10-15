bdc2ff34efaaf34a2fa737f0948347aa5b354079
unknown <fathur.risqo@gmail.com>
2025-10-15 20:51:31 +0700

    Update the height screen size in contact page

--- src/components/ui/contact-section.tsx
+++ src/components/ui/contact-section.tsx
@@ -3,7 +3,7 @@ import { ContactContent } from './contact-content';
 export const ContactSection = () => {
   return (
     <>
-      <div className="mt-22 lg:mt-36 mb-10 md:mb-16 w-full lg:w-4/5">
+      <div className="w-full lg:w-4/5">
         <div className="animate-fade-in">
           <ContactContent />
         </div>
