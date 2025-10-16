8dda33bf9d343ffe716c32ca05b497246bdd9e8d
unknown <fathur.risqo@gmail.com>
2025-10-16 14:27:53 +0700

    Fix form width responsive issue

--- src/components/ui/contact-content.tsx
+++ src/components/ui/contact-content.tsx
@@ -5,7 +5,7 @@ export const ContactContent = () => {
   return (
     <>
       <div className="w-full max-w-5xl mx-auto px-6">
-        <div className="flex justify-between flex-col lg:flex-row gap-6 lg:gap-8 w-full md:w-[442px] lg:w-full mx-auto">
+        <div className="flex justify-between flex-col lg:flex-row gap-6 lg:gap-8 w-full max-w-[442px] md:w-[442px] md:max-w-full lg:w-full mx-auto">
           <Contact />
           <ContactForm />
         </div>
