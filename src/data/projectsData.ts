3d0a22fc9058cc378f3309e38408483f0738f984
nerveign <fathur.risqo@gmail.com>
2026-06-02 10:32:05 +0700

    Feat: update portofolio

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -9,16 +9,28 @@ const url: string = '/works';
 
 export const data: Project[] = [
   {
+    imageURL: `${url}/Portofolio-BJG.png`,
+    title: 'BJG Point of Sales',
+    description: 'Managed operational and transaction processes, developed modules, APIs, and database functionalities to support business workflows and transaction management.',
+    label: ['ExpressJS', 'PostgreSQL'],
+  },
+  {
+    imageURL: `${url}/Portofolio-Haloop.png`,
+    title: 'Haloop.AI Contact Management',
+    description: 'Developed a full-stack contact management platform that helps users organize and manage professional relationships more effectively.',
+    label: ['NextJS', 'ExpressJS', 'PostgreSQL'],
+  },
+  {
     imageURL: `${url}/portofolio-1.png`,
     title: 'PureCycle Mobile UI',
     description: 'Designed and prototyped a mobile application focused on recyclable waste management. Created user flows and interface designs to provide an intuitive user experience.',
     label: ['UI/UX', 'Prototyping'],
   },
   {
-    imageURL: `${url}/portofolio-3.png`,
-    title: 'React Tetris Game',
-    description: 'Built a browser-based implementation of the classic Tetris game using React. Implemented core gameplay mechanics, scoring, and keyboard controls.',
-    label: ['Front End', 'ReactJS'],
+    imageURL: `${url}/portofolio-5.png`,
+    title: 'Friskay Cat Landing Page',
+    description: 'Converted a design mockup into a responsive landing page using React and TypeScript. Focused on pixel-accurate implementation and responsive user interfaces.',
+    label: ['Front End', 'ReactJS', 'Typescript'],
   },
   {
     imageURL: `${url}/portofolio-4.png`,
@@ -27,9 +39,9 @@ export const data: Project[] = [
     label: ['Full Stack', 'ReactJS', 'ExpressJS'],
   },
   {
-    imageURL: `${url}/portofolio-5.png`,
-    title: 'Friskay Cat Landing Page',
-    description: 'Converted a design mockup into a responsive landing page using React and TypeScript. Focused on pixel-accurate implementation and responsive user interfaces.',
-    label: ['Front End', 'ReactJS', 'Typescript'],
+    imageURL: `${url}/portofolio-3.png`,
+    title: 'React Tetris Game',
+    description: 'Built a browser-based implementation of the classic Tetris game using React. Implemented core gameplay mechanics, scoring, and keyboard controls.',
+    label: ['Front End', 'ReactJS'],
   },
 ];
