a7a71cc096c75415204158ceb916f9b041d02172
nerveign <fathur.risqo@gmail.com>
2026-06-02 09:10:16 +0700

    delete debugging

--- src/lib/emailService.ts
+++ src/lib/emailService.ts
@@ -23,12 +23,6 @@ export const sendEmail = async (message: EmailTemplate) => {
       },
     );
 
-    console.log({
-      serviceId: import.meta.env.VITE_SERVICE_ID,
-      templateId: import.meta.env.VITE_TEMPLATE_ID,
-      userId: import.meta.env.VITE_USER_ID,
-      message: message,
-    });
     toast.success('Message sent successfully');
   } catch (error) {
     toast.error('Failed to send message');
