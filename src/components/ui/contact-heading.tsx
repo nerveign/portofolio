ce470f9a72360e910b0f9d94f8cc449b23d9efcd
nerveign <fathur.risqo@gmail.com>
2026-06-03 15:47:09 +0700

    Adjust content on the contact section

--- src/components/ui/contact-heading.tsx
+++ src/components/ui/contact-heading.tsx
@@ -4,15 +4,20 @@ export const Contact = () => {
   return (
     <div>
       <div>
-        <h2 className="font-semibold text-2xl lg:text-5xl">Contact Me</h2>
+        <h2 className="font-semibold text-2xl lg:text-4xl">Contact Me</h2>
         <div className="mt-3 lg:mt-4 text-zinc-600 dark:text-[#A5A3A3]">
           <div className="xl:max-w-5/6">
             <p>For other queries please get in touch with me via email or complete the form.</p>
           </div>
-          <div className="flex gap-2 items-center mt-2 lg:mt-3">
-            <div className="flex gap-2 items-center mt-2">
-              <Mail className="size-5 text-zinc-600 dark:text-white" />
-              <p className="text-sm font-medium dark:text-white">riszqofathur@gmail.com</p>
+          <div className="flex gap-4 items-center mt-2 lg:mt-3">
+            <div className="flex gap-2 md:gap-4 items-center mt-2">
+              <div className="size-10 flex justify-center items-center text-blue-500 bg-blue-500/10 rounded-full p-1">
+                <Mail className="size-5 text-blue-500" />
+              </div>
+              <div className="flex flex-col gap-0.5">
+                <h3 className="font-semibold text-gray-900">Email</h3>
+                <p className="text-sm font-medium text-stone-500 dark:text-white">riszqofathur@gmail.com</p>
+              </div>
             </div>
           </div>
         </div>
