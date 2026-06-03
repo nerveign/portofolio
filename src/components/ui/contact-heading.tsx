6fe67bd3febe700a255ea5a9be2811284a3f90fc
nerveign <fathur.risqo@gmail.com>
2026-06-03 15:53:05 +0700

    Adjust color on contact page

--- src/components/ui/contact-heading.tsx
+++ src/components/ui/contact-heading.tsx
@@ -11,12 +11,12 @@ export const Contact = () => {
           </div>
           <div className="flex gap-4 items-center mt-2 lg:mt-3">
             <div className="flex gap-2 md:gap-4 items-center mt-2">
-              <div className="size-10 flex justify-center items-center text-blue-500 bg-blue-500/10 rounded-full p-1">
-                <Mail className="size-5 text-blue-500" />
+              <div className="size-10 flex justify-center items-center text-blue-500 bg-blue-500/10 dark:bg-[linear-gradient(178deg,#A5D8FF_-172.89%,#0068F0_206.02%)] rounded-full p-1">
+                <Mail className="size-5 text-blue-500 dark:text-white" />
               </div>
               <div className="flex flex-col gap-0.5">
-                <h3 className="font-semibold text-gray-900">Email</h3>
-                <p className="text-sm font-medium text-stone-500 dark:text-white">riszqofathur@gmail.com</p>
+                <h3 className="font-semibold text-gray-900 dark:text-white">Email</h3>
+                <p className="text-sm font-medium text-zinc-600 dark:text-[#A5A3A3]">riszqofathur@gmail.com</p>
               </div>
             </div>
           </div>
