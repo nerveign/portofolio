2f95c67b023e04ca784d3d7200a42918d0775e99
nerveign <fathur.risqo@gmail.com>
2026-06-04 11:22:36 +0700

    Refactor navbar style

--- src/components/ui/menu-list.tsx
+++ src/components/ui/menu-list.tsx
@@ -1,20 +1,24 @@
 import { NavLink } from 'react-router-dom';
 
+const menuItemClass = ({ isActive }: { isActive: boolean }) => {
+  return isActive ? 'text-blue-500 dark:text-white' : 'hover:text-zinc-600 dark:hover:text-zinc-300';
+};
+
 export const MenuList = () => {
   return (
-    <ul className="flex gap-6 text-zinc-500 dark:text-zinc-400 ">
+    <ul className="flex gap-6 text-zinc-400 dark:text-zinc-400 ">
       <li>
-        <NavLink to="/" end className={({ isActive }) => (isActive ? 'text-blue-500 dark:text-white' : 'hover:text-zinc-600 dark:hover:text-zinc-300')}>
+        <NavLink to="/" end className={menuItemClass}>
           Home
         </NavLink>
       </li>
       <li>
-        <NavLink to="/works" className={({ isActive }) => (isActive ? 'text-blue-500 dark:text-white' : 'hover:text-zinc-600 dark:hover:text-zinc-300')}>
+        <NavLink to="/works" className={menuItemClass}>
           Works
         </NavLink>
       </li>
       <li>
-        <NavLink to="/contact" className={({ isActive }) => (isActive ? 'text-blue-500 dark:text-white' : 'hover:text-zinc-600 dark:hover:text-zinc-300')}>
+        <NavLink to="/contact" className={menuItemClass}>
           Contact
         </NavLink>
       </li>
